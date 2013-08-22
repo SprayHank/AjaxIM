3b059f976c6e6f80124830b67dc1286de0a0416f
SprayHank <fantasyoui@gmail.com>
2013-08-22 14:35:01 +0800

    保存

--- ajaxim.php
+++ ajaxim.php
@@ -31,10 +31,10 @@ require_once('libraries/db/' . DB_ENGINE . '.php');
 $db_class = DB_ENGINE . '_Database';
 
 if (!function_exists('class_alias')) {
-    // Allows us to alias Database library classes to standardized names
-    function class_alias($original, $alias) {
-        eval('class ' . $alias . ' extends ' . $original . ' {}');
-    }
+	// Allows us to alias Database library classes to standardized names
+	function class_alias($original, $alias) {
+		eval('class ' . $alias . ' extends ' . $original . ' {}');
+	}
 }
 
 class_alias(DB_ENGINE . '_Database', 'Database');
@@ -45,16 +45,16 @@ class_alias(DB_ENGINE . '_Friend', 'Friend');
 
 // Fix problems with magic_quotes_gpc/sybase
 if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
-    $syb = strtolower(ini_get('magic_quotes_sybase'));
-    if(empty($syb) || $syb == 'off') {
-        foreach($_POST as $key => $val) $_POST[$key] = stripslashes($val);
-        foreach($_GET as $key => $val) $_GET[$key] = stripslashes($val);
-        foreach($_COOKIE as $key => $val) $_COOKIE[$key] = stripslashes($val);
-    } else {
-        foreach($_POST as $key => $val) $_POST[$key] = str_replace("''", "'", $val);
-        foreach($_GET as $key => $val) $_GET[$key] = str_replace("''", "'", $val);
-        foreach($_COOKIE as $key => $val) $_COOKIE[$key] = str_replace("''", "'", $val);            
-    }
+	$syb = strtolower(ini_get('magic_quotes_sybase'));
+	if(empty($syb) || $syb == 'off') {
+		foreach($_POST as $key => $val) $_POST[$key] = stripslashes($val);
+		foreach($_GET as $key => $val) $_GET[$key] = stripslashes($val);
+		foreach($_COOKIE as $key => $val) $_COOKIE[$key] = stripslashes($val);
+	} else {
+		foreach($_POST as $key => $val) $_POST[$key] = str_replace("''", "'", $val);
+		foreach($_GET as $key => $val) $_GET[$key] = str_replace("''", "'", $val);
+		foreach($_COOKIE as $key => $val) $_COOKIE[$key] = str_replace("''", "'", $val);
+	}
 }
 
 // import the server library
@@ -64,18 +64,18 @@ $im = new $im_class();
 $action = preg_replace('/^' . preg_quote($_SERVER['SCRIPT_NAME'], '/') . '\/(.+?)(\?.+)?$/', '\1', $_SERVER['REQUEST_URI']);
 
 if(substr($action, 0, 1) != '_' && method_exists($im, $action))
-    if($action == 'poll') {
-        if($_GET['method'] == 'comet') {
-            $im->poll('comet');
-        } else {
-            print $_GET['callback'] . '(' . json_encode($im->poll($_GET['method'])) . ')';
-        }
-    } else {
-        $execute = call_user_func_array(array($im, $action), $_POST);
-        if($execute)
-            print json_encode($execute !== false ? $execute : array('e'=>'wrong args'));
-    }
+	if($action == 'poll') {
+		if($_GET['method'] == 'comet') {
+			$im->poll('comet');
+		} else {
+			print $_GET['callback'] . '(' . json_encode($im->poll($_GET['method'])) . ')';
+		}
+	} else {
+		$execute = call_user_func_array(array($im, $action), $_POST);
+		if($execute)
+			print json_encode($execute !== false ? $execute : array('e'=>'wrong args'));
+	}
 else
-    print json_encode(array('e'=>'no method'));
+	print json_encode(array('e'=>'no method'));
 
 /* End of ajaxim.php */
