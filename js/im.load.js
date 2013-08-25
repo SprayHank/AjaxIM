500f898b23bc33d5815e0a0932b706f93e294353
SprayHank <fantasyoui@gmail.com>
2013-08-25 13:27:42 +0800

    保存

--- js/im.load.js
+++ js/im.load.js
@@ -1,47 +1,91 @@
 // Automatically load dependencies, in order, if they aren't already loaded.
 // Each array is: [filename, deptest] where deptest is the function to
 // test for the dependency.
-var AjaxIM, AjaxIMLoadedFunction;
+var AjaxIM, AjaxIMLoadedFunction, AjaxIMENV;
 (function() {
-    AjaxIM = {};
+	AjaxIM = {};
 	AjaxIM.loaded = function(f) {
 		AjaxIMLoadedFunction = f;
 	};
+	AjaxIMENV = {
+		jQuery: null,
+		Sizzle: null,
+		JSONP: null,
+		jStore: null,
+		md5: null,
+		AjaxIM: null
+	};
+	var haveRequirejs = (
+		typeof define === "function" &&
+			define.amd &&
+			typeof window['require'] !== 'undefined' &&
+			typeof window['requirejs'] !== 'undefined' &&
+			require === requirejs
+		);
+	//console.log(haveRequirejs);
 
 	var tagsrc =
-		(thistag = document.getElementsByTagName('script'))[thistag.length-1].src;
+		(thistag = document.getElementsByTagName('script'))[thistag.length - 1].src;
 	var jsfolder = tagsrc.replace(/im.load.js([?].+)?/, '');
 	var imfolder = jsfolder.replace(/js\/$/, '');
 
 	var nodehost = '';
 
 	var dependencies = [
-		['jquery-1.3.2.js', function() { return (typeof window['jQuery'] != 'undefined'); }],
-		['sizzle.js', function() { return (typeof window['Sizzle'] != 'undefined'); }],
-		['jquery.jsonp-1.1.0.js', function() { return (typeof jQuery['jsonp'] != 'undefined'); }],
-		['jquery.jstore-all-min.js', function() { return (typeof jQuery['jstore'] != 'undefined'); }],
-		['jquery.md5.js', function() { return (typeof jQuery['md5'] != 'undefined'); }],
-		['im.js', function() { return (typeof window['AjaxIM'] != 'object'); }]
+		['jquery', window['jQuery'], 'undefined', 'jQuery'],
+		['sizzle', window['Sizzle'], 'undefined', 'Sizzle'],
+		['jquery.jsonp', window['jsonp'], 'undefined', 'JSONP'],
+		['jquery.jstore-all-min', window['jstore'], 'undefined', 'jStore'],
+		['jquery.md5', window['md5'], 'undefined', 'md5'],
+		['im', window['AjaxIM'], 'object', 'AjaxIM']
 	];
 
 	var head = document.getElementsByTagName('head')[0];
 
 	(loadDep = function(depPos) {
-		if(depPos >= dependencies.length) { init(); return; }
+		if(depPos >= dependencies.length) {
+			init();
+			return;
+		}
 		var dep = dependencies[depPos];
 
-		if(!dep[1]()) {
-			var newdep = document.createElement('script');
-			newdep.type = 'text/javascript';
-			newdep.src = jsfolder + dep[0];
-
-			newdep.onload = function() { loadDep(depPos + 1); };
-			newdep.onreadystatechange = function () {
-				if (/loaded|complete/.test(this.readyState)) loadDep(depPos + 1);
-			};
-
-			head.appendChild(newdep);
-		} else loadDep(depPos + 1);
+		if(haveRequirejs) {
+			/*require([dep[0]], function(v) {
+				AjaxIMENV[dep[3]] = v;
+				loadDep(depPos + 1);
+			}, function(err) {
+				//The errback, error callback
+				//The error has a list of modules that failed
+				//console.log(err);
+				var failedId = err.requireModules && err.requireModules[0];
+				//console.log(failedId);
+				requirejs.undef(failedId);
+				require([jsfolder + dep[0] + '.js'], function(v) {
+					AjaxIMENV[dep[3]] = v;
+					loadDep(depPos + 1);
+				});
+			});*/
+		} else {
+			if(typeof dep[1] === dep[2]) {
+				var newdep = document.createElement('script');
+				newdep.type = 'text/javascript';
+				newdep.src = jsfolder + dep[0] + '.js';
+				newdep.onload = function() {
+					AjaxIMENV[dep[3]] = dep[1];
+					loadDep(depPos + 1);
+				};
+				newdep.onreadystatechange = function() {
+					if(/loaded|complete/.test(this.readyState)) {
+						AjaxIMENV[dep[3]] = dep[1];
+						loadDep(depPos + 1);
+					}
+				};
+				head.appendChild(newdep);
+			} else {
+				AjaxIMENV[dep[3]] = dep[1];
+				loadDep(depPos + 1);
+			}
+		}
 	})(0);
 
 	var init = function() {
